d408a64a678d00350a55f450799532f3fc2245d2
tmcgahey93 <tmcgahey93@gmail.com>
2025-04-13 15:45:53 -0400

    Added Promtheus Handler

--- main.go
+++ main.go
@@ -4,15 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 
 	http.HandleFunc("/", serviceBHandler)
 
+	// Register /metrics handler to expose Prometheus metrics
+	http.Handle("/metrics", promhttp.Handler())
+
 	fmt.Println("Server Running on port 8081....")
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
+
 func serviceBHandler(w http.ResponseWriter, r *http.Request) {
 	returnMessage := "Hello from Service B"
 	fmt.Fprintln(w, returnMessage)
